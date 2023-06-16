1b14104dea5090a03aa9b15aaf74a9d738f365b8
Randanevo1 <89418410+Randanevo1@users.noreply.github.com>
2023-06-15 20:41:55 -0500

    changed lines to not be foldered

--- Viewer/src/line/line.gd
+++ Viewer/src/line/line.gd
@@ -1,14 +1,11 @@
 extends Control
 class_name c_line
 
-@onready var code:       Label = $VBoxContainer2/MarginContainer/main_cont/code.value
-@onready var indent:     Label = $VBoxContainer2/MarginContainer/main_cont/indent.value
-@onready var parameters: Label = $VBoxContainer2/MarginContainer/main_cont/parameters.value
-@onready var main_cont = $VBoxContainer2/MarginContainer/main_cont
+@onready var code:       Label = $VBoxContainer2/ScrollContainer/MarginContainer/main_cont/code.value
+@onready var indent:     Label = $VBoxContainer2/ScrollContainer/MarginContainer/main_cont/indent.value
+@onready var parameters: Label = $VBoxContainer2/ScrollContainer/MarginContainer/main_cont/parameters.value
+@onready var main_cont = $VBoxContainer2/ScrollContainer
 @onready var id = $VBoxContainer2/expand_container/id
-
-
-var line_dict: Dictionary
 
 
 func _on_expand_container_request_expand():
